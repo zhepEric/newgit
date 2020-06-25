306e66c77d45c7f1bbcd596b817076a677f72b5b
piaozhe <piaozhe@bytedance.com>
2020-06-25 20:25:24 +0800

    update2 TestGit
    Signed-off-by: piaozhe <piaozhe@bytedance.com>

--- newGit/app/src/main/java/com/qf/newgit/TestGit.java
+++ newGit/app/src/main/java/com/qf/newgit/TestGit.java
@@ -3,4 +3,6 @@ package com.qf.newgit;
 public class TestGit {
     public String test = "zzzz";
     public String test1 = "sssss";
+    public String test2 = "tttt";
+    public String test3 = "yyyy";
 }
